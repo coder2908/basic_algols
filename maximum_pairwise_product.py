04bea0ad16e108d30ec3d5b350c5d6b64c3bf7c6
Abhishek Kumar <35439414+coder2908@users.noreply.github.com>
2018-02-14 17:07:51 +0430

    To fin maximum pairwise product between setr of integers.

---
+++ maximum_pairwise_product.py
@@ -0,0 +1,16 @@
+#Uses python 3
+l = int(input())
+str = input()
+str = str.split()
+n = []
+for i in range(0,len(str)):
+    n.append(int(str[i]))
+assert(l == len(n))
+n.sort()
+l = len(n)
+a , b = n[l-1], 0
+for i in range(l-1,0,-1):
+    if (n[i] != a):
+        b = n[i]
+        break
+print(a*b)
