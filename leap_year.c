4dd367482bf0d7482d0fabd94fe7f2d6cee07939
Prakhar Sharan <35136020+itsmeprakhar22@users.noreply.github.com>
2018-10-07 06:58:21 -0400

    Updated for Century year

--- leap_year.c
+++ leap_year.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-/* To check a num is leap year no or not
-A leap year is divided by 4.*/
+/* To check an year is leap year no or not
+A leap year is divisible by 4. A century leap year is divisible by 400.*/
 int main(void)
 {
   int y;
+  printf("Enter the year");
   scanf("%d",&y);
-  if (y % 4 == 0)
+  if (y % 400==0) //For century leap year rules
+    printf("YES");
+  else if(y%100!=0&&y%4==0) //for non-century years
     printf("YES");
   else
     printf("NO");
